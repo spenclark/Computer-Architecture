a47d1d20a6424f4074c2f858a623de64cd2e9931
spenclark <spenclark@hotmail.com>
2020-05-20 11:35:00 -0700

    added MUL to alu method and made load() dynamic

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -20,24 +20,16 @@ class CPU:
         
     def load(self):
         """Load a program into memory."""
-
         address = 0
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(sys.argv[1]) as file:
+            for line in file:
+                line = line.split("#")[0].strip()
+                if line == "":
+                    continue
+                instruction = int(line, 2)
+                self.ram[address] = instruction
+                address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -46,6 +38,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.ram[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
